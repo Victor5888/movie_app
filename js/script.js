2a0b514ef052cace1b64d674d546fd2d98089c0e
Victor5888 <yeboahmensahvictor5888@gmail.com>
2024-06-15 04:24:37 +0100

    updated

--- js/script.js
+++ js/script.js
@@ -2,6 +2,75 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// display movie details
+async function displayMovieDetails() {
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+  div.innerHTML = ` <div class="details-top">
+          <div>
+          ${
+            movie.poster_path
+              ? `<img
+            src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+              : `<img
+          src="../images/no-image.jpg"
+          class="card-img-top"
+          alt="${movie.title}"
+        />`
+          }
+          </div>
+          <div>
+            <h2>${movie?.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+            ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie?.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres
+                .map(
+                  (genre) => `<li>${genre.name}
+                </li>`
+                )
+                .join('')}
+            </ul>
+            <a href="#" target="_blank" class="btn">${movie.homepage}</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span>$${movie.budget}</li>
+            <li><span class="text-secondary">Revenue:</span>$${
+              movie.revenue
+            }</li>
+            <li><span class="text-secondary">Runtime:</span>${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${movie.production_companies
+            .map((company) => `<span>${company.name} </span>`)
+            .join('')}
+          
+          </div>
+        </div>`;
+
+  document.querySelector('#movie-details').appendChild(div);
+  console.log(div);
+}
+
 // Display 20 most popular movies
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular');
@@ -88,10 +157,19 @@ async function fetchAPIData(endpoint) {
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
-
+  showSpinner();
   const data = await response.json();
-
+  hideSpinner();
   return data;
+}
+
+// this is the function to display the spinner while fetching the data
+
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
 }
 
 // Highlight active link
@@ -115,7 +193,7 @@ function init() {
       displayTvShows();
       break;
     case '/movie-details.html':
-      console.log('Movie Details');
+      displayMovieDetails();
       break;
     case '/tv-details.html':
       console.log('TV Details');
